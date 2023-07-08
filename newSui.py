28d7a3d6a431a7f97da124f403e1b39a3f351128
Saeidha <sdhosseinabadi@gmail.com>
2023-07-08 22:24:59 +0330

    fix sui

--- newSui.py
+++ newSui.py
@@ -1,4 +1,5 @@
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -48,81 +49,71 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://sui8192.ethoswallet.xyz/")
 time.sleep(10)
-print("-------1")
 
 
 
 # Simulate pressing the "down" key on the top-level document
 actions = ActionChains(driver)
-print("-------2")
 actions.send_keys(Keys.DOWN).perform()
 
-print("-------3")
 
 wait = WebDriverWait(driver, 20)
 popup_window = wait.until(EC.number_of_windows_to_be(2))
 driver.switch_to.window(driver.window_handles[1])
 
-print("-------4")
 # Find the password input element
 password_input = driver.find_element(By.NAME, 'password')
-print("-------5")
 # Enter the password into the input field
 password_input.send_keys("S12saeed@")
-
-print("-------6")
-# Find the button element
 
 # Find the button element by XPath
 buttonPath = "//button[div[contains(@class, 'truncate')] and div[text()='Unlock Wallet']]"
 wait.until(lambda d: d.find_element(By.XPATH, buttonPath))
 button = driver.find_element(By.XPATH, buttonPath)
-# Click the button
 button.click()
 
-
-# time.sleep(5)
-# button = driver.find_element_by_xpath("//button[text()='Connect Wallet']")
-# button.click()
-# driver.get("chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn/notification.html")
-# password_field = driver.find_element("id", "password")
-# password_field.send_keys("S12saeed")
-# password_field.submit()
-time.sleep(5)
-
-# Simulate pressing the "down" key on the top-level document
-actions = ActionChains(driver)
-print("-------8")
-actions.send_keys(Keys.DOWN).perform()
-
-
+# click Approve
 buttonPath = "//button[div[contains(@class, 'truncate')] and div[text()='Approve']]"
+wait.until(lambda d: d.find_element(By.XPATH, buttonPath))
 button = driver.find_element(By.XPATH, buttonPath)
-
-# Click the button
 button.click()
 
-print("-------9")
-driver.switch_to.window(driver.window_handles[0])
-print("-------10")
+# print("-------9")
+# wait.until(EC.number_of_windows_to_be(2))
+# driver.switch_to.window(driver.window_handles[0])
+# print("-------10")
 time.sleep(2)
 
 
 while True:
+
+    print("-------1")
+    # Get the number of open windows
+    num_windows = len(driver.window_handles)
+    #  switch to main
+    if num_windows > 1:
+        print("-------2")
+        driver.switch_to.window(driver.window_handles[1])
+        driver.close()
+        print("-------3")
+        driver.switch_to.window(driver.window_handles[0])
+    print("-------4")
+    # down button
     actions = ActionChains(driver)
     actions.send_keys(Keys.DOWN).perform()
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.DOWN).perform()
-    print("-------11")
-    wait = WebDriverWait(driver, 20)
+    print("-------5")
+
     popup_window = wait.until(EC.number_of_windows_to_be(2))
     driver.switch_to.window(driver.window_handles[1])
-    print("-------12")
-    time.sleep(10)
+    print("-------6")
+
+     # click Approve
     buttonPath = "//button[div[contains(@class, 'truncate')] and div[text()='Approve']]"
+    wait.until(lambda d: d.find_element(By.XPATH, buttonPath))
     button = driver.find_element(By.XPATH, buttonPath)
-    print("-------14")
     button.click()
-    time.sleep(2)
+    print("-------7")
+
+    time.sleep(1)
 
 # driver.close()
